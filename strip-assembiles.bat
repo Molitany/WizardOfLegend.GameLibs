0f214c4248e9d644101915fa5c5541b72c447fcc
Thor Beregaard <thor@beregaard.dk>
2024-05-19 16:06:56 +0200

    strip and publish gamelibs

--- strip-assembiles.bat
+++ strip-assembiles.bat
@@ -1,7 +1,7 @@
 @echo off 
 
 @REM Add all the assemblies you want to publicize in this list
-set toPublicize=Assembly-CSharp.dll Assembly-CSharp-firstpass.dll
+set toPublicize=Assembly-CSharp.dll Assembly-CSharp-firstpass.dll Rewired_Core.dll UnityEngine.UI.dll UnityEngine.Networking.dll
 
 @REM Add all the assemblies you want to copy as-is to the package in this list
 set dontTouch=
